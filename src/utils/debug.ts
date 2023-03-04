37f42bb279f32a311e04ca5bb5f7b6519e4fa0b6
Hellyson Rodrigo Parteka <hellysonrp@gmail.com>
2023-03-04 13:13:18 -0300

    feat: add time information to debug messages

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,3 +1,11 @@
 import debug from 'debug';
 
+// https://stackoverflow.com/a/54905752
+const origFormatArgs = debug.formatArgs;
+debug.formatArgs = function (args) {
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  args[0] = `${new Date().toUTCString()} ${args[0]}`;
+  origFormatArgs.call(this, args);
+};
+
 export default debug('muse');
